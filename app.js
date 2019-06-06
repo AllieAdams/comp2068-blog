1a55e33eb829e1972425113ff9d488b076dad696
AllieAdams <200380386@student.georgianc.on.ca>
2019-06-06 08:28:35 -0400

    comment

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+require('dotenv').config();
+
+// Connecting to MongoDB cluster with Mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+  auth: {
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  },
+  useNewUrlParser: true
+}).catch(err => console.error(`ERROR: ${err}`));
+
 const express = require(`express`);
 
 const app = express();
+
+//Body Parser which will make reading request bodies MUCH easier
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //our routes
 const routes = require('./routes.js');
@@ -10,6 +29,9 @@ app.use('/', routes);
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use('/css', express.static('assets/stylesheets'));
+app.use('/js', express.static('assets/javascripts'));
+app.use('/images', express.static('assets/images'));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ const app = express();
 
 //Import our page routes
 const pageRoutes = require('./routes/pages');
+const blogRoutes = require('./routes/blogs');
 
 // Reigster our page routes 
 app.use('/', pageRoutes);
+app.use('/blogs', blogRoutes);
 
 //export the changes
 module.exports = app;
